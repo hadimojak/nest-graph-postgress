a72be28e63add03d44f70278953ea0f8b1ff715a
Hadi Arbabi <H.Arbabi@tejaratshayan.com>
2025-11-15 17:15:22 +0330

    mikro-orm fastify

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MikroOrmModule.forRoot()],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
